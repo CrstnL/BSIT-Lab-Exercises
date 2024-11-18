2e26d0ba96b57842daa04862aa07ed26855b0c74
Lacno, Crestine <crestinel123@gmail.com>
2024-11-18 20:40:57 +0800

    Add files via upload

---
+++ Quiz3_loop.java
@@ -0,0 +1,15 @@
+
+package quiz3_loop;
+
+public class Quiz3_loop {
+    public static void main(String[] args) {
+        
+       int x = 10;
+        while (x <= 100) {
+           
+            System.out.println(x);
+             x += 10;
+        }
+    }
+    
+}
